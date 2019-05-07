9b21bcdec34d5cc5de027d08126a13fe59062ea4
Kavin1996 <47286663+Kavin1996@users.noreply.github.com>
2019-05-07 10:10:14 +0530

    Add files via upload
    java File to find the repeating words in a sentance

--- App.java
+++ App.java
@@ -1,27 +1,14 @@
-package sample.check;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+package Sample.Project;
 
 public class App {
-	public static String main(String[] args) {
-		MysqlDataSource dataSource = new MysqlDataSource();
-		JdbcTemplate jdbcTemplate = null;
-		String clientName = null;
-		try {
-			dataSource.setURL("DataBase URL");
-			dataSource.setUser("USERNAME");
-			dataSource.setPassword("PASSWORD");
-			jdbcTemplate = new JdbcTemplate(dataSource);
-			try {
-				clientName = jdbcTemplate.queryForObject("select client name from client where id=2", String.class);
-			} catch (Exception e) {
-				e.printStackTrace();
+	public static void main(String args[]) throws Exception {
+		String sentance = "kavindjnfkjnKavinbfjnfKavinbdojewkavin".toLowerCase();
+		int count = 0;
+		for (int a = 0; a <= sentance.length() - 1; a++) {
+			if (sentance.substring(a).startsWith("kavin")) {
+				count++;
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		return clientName;
+		System.out.println(count);
 	}
 }
